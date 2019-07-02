6ea9fca555e5e46b09a8d148fd5ea3dca624a8dc
Martin Polder <martinpolder@gmail.com>
2019-07-02 11:04:11 -0400

    variables & pseudo coding

---
+++ assets/js/game.js
@@ -0,0 +1,31 @@
+// VARIABLES
+
+// randomly generated number
+var targetNum = [];
+
+// counter variable
+var counter = 0;
+
+// Random numbers generated for each crystal
+var cOne = [];
+var cTwo = [];
+var cThree = [];
+var cFour = [];
+
+// reset function
+
+// loop:
+        // randomly generate value for crystal one 
+        // apply it to crystal one using .attr
+
+        // ditto for crystals two, three and four.
+
+
+
+// game scoring
+        //add each attributed value of the crystal added to the score counter when clicked
+
+// win/loss
+        //if the counter reaches the value of the randomly generated target number the player wins, display win message, game resets.
+
+        //if the counter goes over the value of the target number the player loses, display loss message, game resets.
